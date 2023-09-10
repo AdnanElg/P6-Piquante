e2854ae4c615b1e5d88d46fba4e7bc7438bb784e
El Ghalbzouri Adnan <elghalbzouriadnan@gmail.com>
2023-09-10 23:46:13 +0200

    FIX

--- backend/app.js
+++ backend/app.js
@@ -53,7 +53,9 @@ app.use(helmet({crossOriginResourcePolicy: false,}));
 
 //Routes :
 app.use('/images', express.static(path.join(__dirname,'images')));
-app.use('/api/auth', userRoutes);
+setTimeout(() => {
+    app.use('/api/auth', userRoutes);
+}, 10000);
 app.use('/api/sauces', sauceRoutes);
 
 
