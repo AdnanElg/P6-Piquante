114056150135341317d71d3eacde3e15a63e2745
El Ghalbzouri Adnan <elghalbzouriadnan@gmail.com>
2023-09-22 16:29:00 +0200

    FIX

--- backend/mongoDB/db.js
+++ backend/mongoDB/db.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const dotenv = require("dotenv").config();
 
 //Conection de mongoDB à l'API grâce à mongoose :
-mongoose.connect(`mongodb+srv://p6-piquante:ca8v1OrQMmdSgN3W@cluster0.1rw0dxd.mongodb.net/?retryWrites=true&w=majority`,
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB c\'est connecté avec succée !'))
     .catch(() => console.log('MongoDB n\'est pas connecté !'))
