e29e9af1ca51fde8f3ce617451b78c1cc811f124
El Ghalbzouri Adnan <elghalbzouriadnan@gmail.com>
2023-10-03 11:43:56 +0200

    FIX

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const normalizePort = val => {
 };
 
 
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || 'https://piquante-bd.vercel.app/');
 app.set('port', port);
 
 
