4e41cdfcc77304985399c16436b9218770317fb5
El Ghalbzouri Adnan <elghalbzouriadnan@gmail.com>
2023-10-03 15:41:02 +0200

    FIX

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -7,7 +7,7 @@ const sauceSchema = mongoose.Schema({
     manufacturer: {type: String, required: true},
     description: {type: String, required: true},
     mainPepper: {type: String, required: true}, 
-    imageUrl: {type: String}, 
+    imageUrl: {type: String, required: true}, 
     heat: {type: Number, default: 0}, 
     likes: {type: Number, default: 0},
     dislikes :  {type: Number, default: 0},
